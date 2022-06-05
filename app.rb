4c98ef8dc86ff5596ec725d36aebfe1a03987dd3
Justus-Onyancha <justus.onyancha@moringaschool.com>
2022-06-05 11:17:57 +0300

    Done.

---
+++ app.rb
@@ -0,0 +1,19 @@
+
+# app.rb
+
+puts "Hello World!"
+
+puts("Hello world!")
+
+print("Pass this test, please.")
+p("Hello world!")
+
+pp("Hello world!")
+
+puts [1,2,3]
+
+p [1,2,3]
+
+# one more method, pp, for "pretty-printing" complex data, such as nested arrays and hashes
+
+pp [{id: 1, name: "John"}, {id: 2, name: "Jane"}]
